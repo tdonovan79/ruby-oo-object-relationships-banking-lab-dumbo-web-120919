722f37b3b158612b8148bfe4fc382a96ceacaafe
tdonovan79 <tdonovan79@gmail.com>
2019-12-12 15:57:07 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+    #deposit money into account
+    def deposit(amount)
+        @balance += amount
+    end
+    #remove money from account
+    def withdraw(amount)
+        @balance -= amount
+    end
+    #return balance in account
+    def display_balance
+        return "Your balance is $#{@balance}."
+    end
+    #returns true if account is open and balance is >0
+    def valid?
+        @balance > 0 && @status == "open"
+    end
+    #changes status to closed
+    def close_account
+        @status = "closed"
+    end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,51 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_accessor :status, :amount, :sender, :receiver
+
+  @@completed_transactions = []
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  #returns array of transactions
+  def self.completed_transactions
+    @@completed_transactions
+  end
+  #checks to see if both accounts are valid
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+  #transfer money from sender to receiver and record transaction if valid
+  def execute_transaction
+    if !self.valid? || @status != "pending" || @sender.balance < @amount
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    else
+      transfer_money(@sender, @receiver, @amount)
+      record_transfer
+    end
+  end
+  #check to see if transaction can go through
+  def transfer_money(from, to, amount)
+    from.withdraw(amount)
+    to.deposit(amount)
+  end
+  #record transaction and chacge status to complete
+  def record_transfer
+    @status = "complete"
+    @@completed_transactions << self
+  end
+  #reverse last transfer
+  def reverse_transfer
+    if Transfer.completed_transactions.last.status != "complete"
+      "This transaction has not been completed."
+    else
+      transfer_money(Transfer.completed_transactions.last.receiver, Transfer.completed_transactions.last.sender, Transfer.completed_transactions.last.amount)
+      Transfer.completed_transactions.last.status = "reversed"
+    end
+  end
 end
